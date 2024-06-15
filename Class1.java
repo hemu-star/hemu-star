eb3416bc011adfea4dd123c895efae521e152daf
hemu-star <hsa18011988@gmail.com>
2024-06-15 18:09:45 +0530

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,24 @@
+public class Class1
+{
+public void meth1( )
+{
+int a=10;
+int b=20;
+ClassA obj=new ClassA();
+obj.meth2();
+System.out.println("addition :" +(a+b));
+}
+public void meth2()
+{
+float a=2.5;
+float b=6.9;
+System.out.println("addition :" +(a+b));
+}
+public static void main(String st[])
+{
+System.out.println("Starts");
+obj.meth1();
+}
+}
+
+
